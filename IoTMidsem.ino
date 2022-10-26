a380c546eb556eb811a848ed7c90473cdb575fb7
KelvinTichana-Creator <kelvin.tichana@ashesi.edu.gh>
2022-10-26 09:28:44 +0000

    First commit

--- IoTMidsem.ino
+++ IoTMidsem.ino
@@ -1,15 +1,20 @@
+#include <LiquidCrystal_I2C.h>
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <liquidCrystal_I2C.h>
 #define led 2
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x3F, 16 column and 2 rows
 
 #define TRIG_PIN 26 // ESP32 pin GIOP26 connected to Ultrasonic Sensor's TRIG pin
 #define ECHO_PIN 25 // ESP32 pin GIOP25 connected to Ultrasonic Sensor's ECHO pin
 #define REDLED 19
-#define CW 15
+#define CW 33
 #define CCW 4
+float duration_us, distance_cm, minimum_depth, tank_height, current_level;
+
 const char* ssid = "DZ";
 const char* password = "0011223344";
 
@@ -20,6 +25,18 @@ WebServer server(80);
 void handleManualON() {
   digitalWrite(led, HIGH );
   server.send(200, "text/plain", "turn motor on!");
+  digitalWrite(CW,HIGH); //Motor runs clockwise//
+
+  delay(1000); //for 1 second//
+
+  digitalWrite(CW, LOW); //Motor stops//
+
+  digitalWrite(CCW, HIGH);//Motor runs counter-clockwise//
+
+  delay(1000); //For 1 second//
+
+  digitalWrite(CCW, LOW); //Motor stops//
+  measureDistance();
 
   //digitalWrite(led, 0);
 }
@@ -28,6 +45,8 @@ void handleManualOFF() {
   digitalWrite(led, LOW );
   server.send(200, "text/plain", "turn motor off!");
   //digitalWrite(led, 0);
+  digitalWrite(CCW, LOW);
+  digitalWrite(CCW, LOW);
 }
 
 void handleAUTO() {
@@ -49,16 +68,19 @@ void handleAUTO() {
   delay(1000); //For 1 second//
 
   digitalWrite(CCW, LOW); //Motor stops//
+  measureDistance();
+  if(distance_cm>179){
+    digitalWrite(CCW, LOW);
+    digitalWrite(CCW, LOW);
+    
+  }
 
 } 
   
 
   //digitalWrite(led, 0);
   }
-  while(distance_cm>180){
-    digitalWrite(CCW, LOW);
-    digitalWrite(CCW, LOW);
-  }
+  
 }
 
 
@@ -113,12 +135,12 @@ void loop(void) {
   server.handleClient();
   delay(2);//allow the cpu to switch to other tasks
   // generate 10-microsecond pulse to TRIG pin
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  //digitalWrite(TRIG_PIN, HIGH);
+  //delayMicroseconds(10);
+  //digitalWrite(TRIG_PIN, LOW);
 
   // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(ECHO_PIN, HIGH);
+  //duration_us = pulseIn(ECHO_PIN, HIGH);
 
   // calculate the distance
   distance_cm = 0.017 * duration_us;
@@ -132,4 +154,19 @@ void loop(void) {
   lcd.print(distance_cm); /// subtract this distance from length of tank
 
   delay(500);
+}
+float measureDistance(){
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+  current_level = tank_height - distance_cm;
+  return distance_cm;
+
 }
